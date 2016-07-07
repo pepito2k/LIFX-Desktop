0a6886067e639386ea3fbb6b21a8ceada63c5d7d
Gonzalo Robaina <gonzalor@gmail.com>
2016-07-07 00:30:44 -0300

    Initial Commit

---
+++ LIFX Desktop/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  LIFX Desktop
+//
+//  Created by Gonzalo Robaina on 7/7/16.
+//  Copyright © 2016 Gonzalo Robaina. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
